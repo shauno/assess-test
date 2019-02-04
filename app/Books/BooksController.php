b01c5502e9704534869d4af10570bb93e5809c2b
Shaun Alberts <shaunalberts@gmail.com>
2019-02-04 10:17:29 +0200

    Show book listing

--- app/Books/BooksController.php
+++ app/Books/BooksController.php
@@ -8,13 +8,31 @@ use Slim\Views\PhpRenderer;
 
 class BooksController
 {
+    /**
+     * @var PhpRenderer
+     */
+    protected $renderer;
+
+    /**
+     * @var \PDO
+     */
+    protected $db;
+
     public function __construct()
     {
         $this->renderer = new PhpRenderer('../app/');
+        $this->db = new \PDO('mysql:host=database;dbname=assess_db', 'root', 'secret');
+        $this->db->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
     }
 
     public function index(Request $request, Response $response)
     {
-        return $this->renderer->render($response, 'Books/templates/list.php', []);
+        $books = $this->db->query('SELECT * FROM books')
+            ->fetchAll();
+
+        return $this->renderer->render($response, 'Books/templates/list.php', [
+            'books' => $books,
+            'db' => $this->db,
+        ]);
     }
 }
--- app/Books/templates/list.php
+++ app/Books/templates/list.php
@@ -1,1 +1,22 @@
-Book list
+<?php
+/**
+ * @var array $books
+ * @var PDO $db
+ */
+?>
+
+<table>
+    <tr>
+        <th>Title</th>
+        <th>Author</th>
+    </tr>
+    <?php foreach ($books as $book): ?>
+        <tr>
+            <td><?= $book['title'] ?></td>
+            <?php
+            $author = $db->query('SELECT * FROM authors WHERE id = '.$book['author_id'])->fetch();
+            ?>
+            <td><?= $author['first_name'] ?> <?= $author['last_name'] ?></td>
+        </tr>
+    <?php endforeach; ?>
+</table>
